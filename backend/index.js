e13793c223d3de46286d8130925ab669eba2199c
G1U5 <isaiasgius@outlook.es>
2025-12-22 17:17:47 -0300

    [ADD] config/db y conectamos la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +14,9 @@ app.get("/", (req, res) => {
   res.send("API funcionando");
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto 4000");
 });
+
+console.log("Variable de entorno MONGO_URI:", process.env.MONGO_URI);
