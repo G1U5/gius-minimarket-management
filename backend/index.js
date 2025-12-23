47fe36cfb7a3a30574168d53ed97ec88c8f3e300
G1U5 <isaiasgius@outlook.es>
2025-12-23 18:17:48 -0300

    refactoricé el index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
 
 const app = express();
 
+// Conectar a la base de datos
 connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Rutas
 app.use("/api/auth", require("./routes/auth.routes"));
-app.get("/", (req, res) => {
-  res.send("API funcionando");
-});
 
+// Iniciar el servidor
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
-console.log("Variable de entorno MONGO_URI:", process.env.MONGO_URI);
-
-const User = require("./models/User");
-console.log("Modelo User cargado:", User);
