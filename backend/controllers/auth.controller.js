f8fb571d7d331477a908e93b956cee58dde32fa3
G1U5 <isaiasgius@outlook.es>
2026-01-13 18:45:09 -0300

    [FIX] error de sintaxis en el .env jwt devolvía undefined

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -40,6 +40,8 @@ const registerUser = async (req, res) => {
   }
 };
 
+const jwt = require("jsonwebtoken");
+
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -48,9 +50,38 @@ const loginUser = async (req, res) => {
     if (!email || !password) {
       return res.status(400).json({ msg: "Email y contraseña obligatorios" });
     }
-  } catch (error) {}
+
+    // Search User
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Credenciales inválidas" });
+    }
+    // Check Password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Credenciales inválidas" });
+    }
+    // Generate Token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+    // Res
+    res.json({
+      msg: "Login exitoso",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Error del servidor" });
+  }
 };
 
 module.exports = {
   registerUser,
+  loginUser,
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../controllers/auth.controller");
+const { registerUser, loginUser } = require("../controllers/auth.controller");
 const router = express.Router();
 
 // REGISTER
 router.post("/register", registerUser);
-
+// Login
+router.post("/login", loginUser);
 module.exports = router;
